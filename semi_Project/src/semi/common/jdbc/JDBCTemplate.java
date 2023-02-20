70092287ce79d9d64077600947afb3e2c6d897a1
uzinnn <wgdfs59@naver.com>
2023-02-20 15:22:12 +0900

    connection

---
+++ semi_Project/src/semi/common/jdbc/JDBCTemplate.java
@@ -0,0 +1,35 @@
+package semi.common.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCTemplate {
+	
+	public static Connection getConnetcion() {
+		
+	String driver = "oracle.jdbc.OracleDriver";
+	String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	String user = "MUSIC";
+	String pw ="MUSIC";
+	Connection conn =null;
+	
+	try {
+		Class.forName(driver);
+		conn = DriverManager.getConnection(url, user,pw);
+	
+	} catch (SQLException e) {
+		e.printStackTrace();
+		
+	} catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	}
+	
+	if(conn != null) System.out.println("DB 연결 성공!!");
+	else  System.out.println("!!!!!!!!!DB 연결 실패!!");
+
+		return conn;
+	}
+	
+}
+
