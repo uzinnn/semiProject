46d3d085c6fd841e538a9ad04441b9b1028f147f
uzinnn <wgdfs59@naver.com>
2023-02-22 15:18:44 +0900

    로그인

---
+++ semi_Project/src/semi/song/controller/LoginController.java
@@ -0,0 +1,26 @@
+package semi.song.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/login")
+public class LoginController {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("WEB-INF/view/index.jsp").forward(request, response);
+		
+	
+	}
+	
+	
+	
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("WEB-INF/view/index.jsp").forward(request, response);
+		
+	
+}
+}
